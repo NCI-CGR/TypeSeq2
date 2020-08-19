69e5eab477dfbfc02a7d2806521d0288c51a84bb
Ashastry2 <amulyashastry@gmail.com>
2020-08-19 12:56:10 -0400

    updates to pull from new docker

--- inst/typeseq2/launch.sh
+++ inst/typeseq2/launch.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -x
 # TypeSeq2 HPV
-VERSION="2.1912.2601"
+VERSION="2.2008.2601"
 #autorundisable
 echo Pipeline version $VERSION
 
 ln ../../*.bam ./
 
 docker run -i -v $(pwd):/mnt -v /mnt:/user_files \
-    cgrlab/typeseqhpv:development_191226 \
+    cgrlab/typeseqhpv:development_stable_191226 \
         Rscript /TypeSeqHPV/workflows/TypeSeq2.R \
         --is_torrent_server yes \
         --config_file config_file.csv \
