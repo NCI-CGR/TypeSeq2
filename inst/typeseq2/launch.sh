10df8c27686798e129f0569767aa99d420e6e80c
Ashastry2 <amulyashastry@gmail.com>
2020-08-21 12:03:08 -0400

    Chaning repo for typeseq

--- inst/typeseq2/launch.sh
+++ inst/typeseq2/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 # TypeSeq2 HPV
-VERSION="2.1912.2601"
+VERSION="2.2008.2101"
 #autorundisable
 echo Pipeline version $VERSION
 
@@ -9,7 +9,7 @@ ln ../../*.bam ./
 
 docker run -i -v $(pwd):/mnt -v /mnt:/user_files \
     cgrlab/typeseqhpv:development_191226 \
-        Rscript /TypeSeqHPV/workflows/TypeSeq2.R \
+        Rscript /TypeSeqHPV2/workflows/TypeSeq2.R \
         --is_torrent_server yes \
         --config_file config_file.csv \
         --barcode_file barcodes.csv \
