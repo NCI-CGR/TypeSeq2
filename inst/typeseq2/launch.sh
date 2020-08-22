c756b6d2acc18a6db4498081f83490af73f6f7d7
Ashastry2 <amulyashastry@gmail.com>
2020-08-21 21:22:15 -0400

    Chaging this to pull from development. Undo this after the merge is complete. This changes the docker name

--- inst/typeseq2/launch.sh
+++ inst/typeseq2/launch.sh
@@ -8,8 +8,8 @@ echo Pipeline version $VERSION
 ln ../../*.bam ./
 
 docker run -i -v $(pwd):/mnt -v /mnt:/user_files \
-    cgrlab/typeseqhpv:development_stable_191226 \
-        Rscript /TypeSeqHPV/workflows/TypeSeq2.R \
+    cgrlab/typeseqhpv:development_191226 \
+        Rscript /TypeSeqHPV2/workflows/TypeSeq2.R \
         --is_torrent_server yes \
         --config_file config_file.csv \
         --barcode_file barcodes.csv \
