73c6fa6b31de00771882168747867e1631b5c8d8
Ashastry2 <amulyashastry@gmail.com>
2020-08-24 17:06:48 -0400

    Changing repo name for methylation dev

--- inst/methylation/launch.sh
+++ inst/methylation/launch.sh
@@ -9,7 +9,7 @@ ln ../../*.bam ./
 
 docker run -i -v $(pwd):/mnt -v /mnt:/user_files \
     cgrlab/typeseqhpv:development_191226 \
-        Rscript /TypeSeqHPV/workflows/ion_methyl_workflow.R \
+        Rscript /TypeSeqHPV2/workflows/ion_methyl_workflow.R \
         --is_torrent_server yes \
         --config_file config_file.csv \
         --barcode_file barcodes.csv \
