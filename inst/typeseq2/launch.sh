9162ab79e7567ebab3f4df85ebd096112e8e5ce0
Ashastry2 <amulyashastry@gmail.com>
2021-06-23 16:26:07 -0400

    Adding all the metrics files required to working directory

--- inst/typeseq2/launch.sh
+++ inst/typeseq2/launch.sh
@@ -6,6 +6,13 @@ VERSION="2.2008.2101"
 echo Pipeline version $VERSION
 
 ln ../../*.bam ./
+cp ../../raw_peak_signal ./
+cp ../../sigproc_results/analysis.bfmask.stats ./
+cp ../../basecaller_results/BaseCaller.json ./
+cp ../../basecaller_results/datasets_basecaller.json ./
+cp ../../basecaller_results/ionstats_tf.json ./
+cp ../../ionstats_alignment.json ./
+
 
 docker run -i -v $(pwd):/mnt -v /mnt:/user_files \
     cgrlab/typeseqhpv:development_191226 \
