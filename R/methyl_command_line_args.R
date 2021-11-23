6f02313a7d2c62b8fda0a678d189f6dbfd2f4e02
Wei Zhu <zhuw10@nih.gov>
2021-11-22 21:22:25 -0500

    feat(methyl_command_line_args.r): move the config files to the pluginMedia
    re #80

--- R/methyl_command_line_args.R
+++ R/methyl_command_line_args.R
@@ -5,20 +5,13 @@ get_command_line_args <- function(args_df){
     library(optigrab)
 
 if ( args_df$is_torrent_server == "yes") {
-
-    plugin_json = fromJSON(file("./startplugin.json"), simplifyDataFrame = TRUE, simplifyMatrix = TRUE)
-
-    # config file
-    data_frame(values = plugin_json$pluginconfig$config_file) %>%
-        mutate(values = str_replace(values, "\n", "" )) %>%
-        separate(col = values, sep = ",", into = c("key", "value")) %>%
-        write_csv("config_file.csv")
+    args_df$config_file <- "/user_files/configs/config_file.csv"
 }
 
 config_file_df = read_csv(args_df$config_file, col_names = c("key", "value")) %>%
     map_if(is.factor, as.character) %>%
     as_tibble() %>%
-    mutate(value = paste0("/user_files/", value)) %>%
+    mutate(value = paste0("/user_files/configs/", value)) %>%
     glimpse()
 
 new_args_df = args_df %>%
