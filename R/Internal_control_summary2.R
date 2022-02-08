3dce6885decbae9c4afeb79a8efa94a35f527635
Wei Zhu <zhuw10@nih.gov>
2022-02-08 09:38:01 -0500

    feat(r/internal_control_summary2.r): for the summary table at the batch level
    Replace the file R/Internal_control_summary.R
    
    re #86

---
+++ R/Internal_control_summary2.R
@@ -0,0 +1,23 @@
+#'
+#'control_summary
+
+#Using detailed_pn_matrix, manifest and control results
+
+Internal_control_summary2 <- function(detailed_pn_matrix_for_report,manifest,control_for_report,specimen_control_defs, for_batch=F){
+
+    # use the code developed for collect_metrics
+    # Add project for Batch level 
+    t4 <- .internal_control_summary(detailed_pn_matrix_for_report,manifest, specimen_control_defs, for_batch)
+    
+    t4 %>% pandoc.table(caption = "Internal Control Summary")
+    
+    #table5 
+    t5 <- .control_sumamry(control_for_report, specimen_control_defs, for_batch)
+    
+    # Introduce spaces to enable multiple lines
+    t5 %>% rename_all(~ gsub("_", " ", .)) %>% pandoc.table(caption = "Control Summary", split.table=100, split.cells=12, style="multiline")
+}
+
+
+
+
