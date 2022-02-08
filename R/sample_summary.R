4f3ebe7ef826e9658ba186f9d34e5c1b1912ccd2
Wei Zhu <zhuw10@nih.gov>
2022-02-08 10:09:56 -0500

    perf(r/sample_summary.r): rewrite the function to make it clear

--- R/sample_summary.R
+++ R/sample_summary.R
@@ -6,26 +6,28 @@
 
 sample_summary <- function(df){
 
-    sampleSummary = df %>%
-        group_by(Project) %>%
-        mutate(numSamplesTested = n()) %>%
-        group_by(Project, numSamplesTested, human_control) %>%
-        summarize(count = n()) %>%
-        transform(human_control = as.character(human_control)) %>%   
-        mutate(human_control = ifelse(str_detect(human_control,'pass'),"pass",human_control)) %>%  
-        ungroup() %>% group_by(Project,human_control) %>% 
-        mutate(sum = sum(count)) %>% select(-count) %>% 
-        distinct() %>% 
-        spread(human_control, sum, fill=0) %>%
-        select(Project_ID = Project, `Number Samples Tested` = numSamplesTested, everything()) %>%
-        mutate(`Number Passed` = ifelse("pass" %in% colnames(.),pass,0)) %>%
-        mutate(`Number Failed` = ifelse("failed_to_amplify" %in% colnames(.),failed_to_amplify,0)) %>%
-        arrange(Project_ID) %>%
-        mutate(`Perc Passed` = ifelse(`Number Passed` != 0,paste0(round(`Number Passed`/`Number Samples Tested` * 100, digits=2), "%"),0))  %>%
-        mutate(`Perc Failed` = ifelse(`Number Failed` != 0,paste0(round(`Number Failed`/`Number Samples Tested` * 100, digits=2), "%"),0)) %>%
-        select(Project_ID,`Number Samples Tested`,`Number Passed`,`Number Failed`,`Perc Passed`,`Perc Failed`) 
+    ## sampleSummary = df %>%
+    ##     group_by(Project) %>%
+    ##     mutate(numSamplesTested = n()) %>%
+    ##     group_by(Project, numSamplesTested, human_control) %>%
+    ##     summarize(count = n()) %>%
+    ##     transform(human_control = as.character(human_control)) %>%   
+    ##     mutate(human_control = ifelse(str_detect(human_control,'pass'),"pass",human_control)) %>%  
+    ##     ungroup() %>% group_by(Project,human_control) %>% 
+    ##     mutate(sum = sum(count)) %>% select(-count) %>% 
+    ##     distinct() %>% 
+    ##     spread(human_control, sum, fill=0) %>%
+    ##     select(Project_ID = Project, `Number Samples Tested` = numSamplesTested, everything()) %>%
+    ##     mutate(`Number Passed` = ifelse("pass" %in% colnames(.),pass,0)) %>%
+    ##     mutate(`Number Failed` = ifelse("failed_to_amplify" %in% colnames(.),failed_to_amplify,0)) %>%
+    ##     arrange(Project_ID) %>%
+    ##     mutate(`Perc Passed` = ifelse(`Number Passed` != 0,paste0(round(`Number Passed`/`Number Samples Tested` * 100, digits=2), "%"),0))  %>%
+    ##     mutate(`Perc Failed` = ifelse(`Number Failed` != 0,paste0(round(`Number Failed`/`Number Samples Tested` * 100, digits=2), "%"),0)) %>%
+    ##     select(Project_ID,`Number Samples Tested`,`Number Passed`,`Number Failed`,`Perc Passed`,`Perc Failed`) 
       #  mutate(`Perc Passed` = paste0(round(pass/numSamplesTested) %>%
       #  mutate(`Perc Failed` = paste0(round(failed_to_amplify/numSamplesTested * 100, digits=2), "%"))
+
+    sampleSummary <- df %>% group_by(Project) %>% summarize(n=n(), pass_n = sum (str_detect(human_control,'pass')), fail_n= sum (str_detect(human_control,'failed_to_amplify')), pass_perc = fmt_perc(pass_n/n), fail_perc= fmt_perc(fail_n/n)) %>%  select(Project_ID=Project,`Number Samples Tested`=n,`Number Passed`=pass_n,`Number Failed`=fail_n, `Perc Passed`=pass_perc,`Perc Failed`=fail_perc) 
         
     panderOptions("table.split.table", 100)
     panderOptions("table.split.cells", 6)
