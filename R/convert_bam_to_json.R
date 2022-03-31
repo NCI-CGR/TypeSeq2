7b6269e24a29959bf89fba08c691161b91bff83d
weizhu365 <4519424+weizhu365@users.noreply.github.com>
2022-03-31 11:10:28 -0400

    perf(R/): Remove 29 R files under R/

--- R/convert_bam_to_json.R
+++
@@ -1,7 +0,0 @@
-#+
-
-convert_bam_to_json <- function(path, bam_dir){
-  require(dplyr)
-  system(paste0("/home/adam/sambamba view --nthreads=8 --format=json ", bam_dir, "/", path, " -o ", path,".json"))
-  df = data_frame(path = unlist(path))
-}
