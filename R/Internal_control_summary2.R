70a542524f46b819e57288ea848d7fae77f35f3a
weizhu365 <4519424+weizhu365@users.noreply.github.com>
2022-04-13 17:00:57 -0400

    fix(R/Internal_control_summary2.R): to address the fuzzy join issue with the summary of the control samples

--- R/Internal_control_summary2.R
+++ R/Internal_control_summary2.R
@@ -3,16 +3,18 @@
 
 #Using detailed_pn_matrix, manifest and control results
 
+### Note that specimen_control_defs is not needed any more
+
 Internal_control_summary2 <- function(detailed_pn_matrix_for_report,manifest,control_for_report,specimen_control_defs, for_batch=F){
     require(pander)
     # use the code developed for collect_metrics
     # Add project for Batch level 
-    t4 <- .internal_control_summary(detailed_pn_matrix_for_report,manifest, specimen_control_defs, for_batch)
+    t4 <- .internal_control_summary(detailed_pn_matrix_for_report, manifest, control_for_report, for_batch)
     
     t4 %>% pandoc.table(caption = "Internal Control Summary")
     
     #table5 
-    t5 <- .control_sumamry(control_for_report, specimen_control_defs, for_batch)
+    t5 <- .control_summary(control_for_report, for_batch)
     
     # Introduce spaces to enable multiple lines
     t5 %>% rename_all(~ gsub("_", " ", .)) %>% pandoc.table(caption = "Control Summary", split.table=100, split.cells=12, style="multiline")
