09a75c5de0f67e8b5cb78016474107bf5c67eae2
Wei Zhu <zhuw10@nih.gov>
2022-05-17 15:51:46 -0400

    feat(r/plate_map.r): rewrite the function plate_map with a new plate map for overall_qc
    re #90

--- R/plate_map.R
+++ R/plate_map.R
@@ -1,257 +1,50 @@
-#library(gtools)
-#library(gridExtra)
-library(dplyr)
-
-
-#Date of completion - 02/07/2020
-#This scripts creates plate maps for 2 internal control types(ASIC and B2M) and batch controls.
-#Takes in manifest, detailed_pn_matrix_for_report and control_results.
-
-
-#Plate map for ASICs
+# redefine the function plate_map 
 plate_map <- function(manifest,detailed_pn_matrix_for_report, specimen_control_defs,control_for_report){
+    well_num = seq(1,12,length.out = 12)  
+    well_ID = LETTERS[1:8]
+    empty_wells = as.data.frame(expand.grid(rownum=well_ID, colnum= well_num,stringsAsFactors = F))
 
-  plate_data <- manifest %>%
-    mutate(barcode = paste0(BC1,BC2)) %>%
-    full_join(specimen_control_defs %>% select(Control_Code,Control_type) %>%
-              mutate(Owner_Sample_ID = Control_Code),
-              by = "Owner_Sample_ID") %>%
-    inner_join(detailed_pn_matrix_for_report, by = c("barcode","Owner_Sample_ID")) %>%
-    gather(type,status,starts_with("ASIC")) %>%
-    mutate(count = ifelse(status == "pos",1,0)) %>%
-    select(barcode,Owner_Sample_ID,Assay_SIC,Assay_Well_ID,Assay_Plate_Code,Control_Code,type,status,count, Assay_Batch_Code) %>%
-    group_by(barcode,Owner_Sample_ID,Assay_Plate_Code, Assay_Batch_Code) %>%
-    mutate(sum_count = sum(count)) %>%
-    select(-count)%>%
-    spread(type,status) %>%
-    mutate(color = "white") %>% 
-    mutate(color = ifelse(sum_count == 0,"0/3_present",color)) %>%
-    mutate(color = ifelse(sum_count == 1,"1/3_present",color)) %>%
-    mutate(color = ifelse(sum_count == 2,"2/3_present",color)) %>%
-    mutate(color = ifelse(sum_count == 3,"3/3_present",color)) %>%
-    mutate(Control_Code = ifelse(is.na(Control_Code),"sample","control")) %>%
-    separate(Assay_Well_ID,c("rownum","colnum"),sep =1) %>%
-    drop_na()
+    # Note tha data contains multiple batch
+    dat <- manifest %>% 
+        mutate(barcode = paste0(BC1,BC2)) %>% 
+        separate(Assay_Well_ID,c("rownum","colnum"),sep =1) %>%
+        select(barcode, rownum, colnum, Assay_Batch_Code, Assay_Plate_Code) %>% 
+        left_join(control_for_report %>% select(barcode, control_result, Control_type)) %>% 
+        inner_join(detailed_pn_matrix_for_report %>% select(barcode, starts_with("ASIC."), overall_qc=overall_qc) )%>%
+        mutate(Control_Code = case_when(
+            is.na(Control_type) ~ "sample",
+            TRUE ~ "control"
+        )) %>% 
+        mutate(ASIC_cnt = rowSums( select(., starts_with("ASIC.")) %>% mutate_all(~ .=="pos"))) %>% 
+        mutate(ASIC_status = sprintf("%s/3_present", ASIC_cnt)) %>% 
+        mutate(overall_qc = as.character(overall_qc)) %>% 
+        mutate(control_status = ifelse(Control_Code=="control",  paste(Control_type, control_result, sep="_"), "sample"))
 
-  
-#Create empty wells to make sure we get a proper 96 well plate
 
-well_num = seq(1,12,length.out = 12)  
-well_ID = LETTERS[1:8]
-empty_wells = as.data.frame(expand.grid(rownum=well_ID, colnum= well_num,stringsAsFactors = F))
+    # Plot for ASIC
+    asic_color <- scale_color_manual(name="ASIC status", values = c("0/3_present" = "red","1/3_present" = "yellow","2/3_present"="orange","3/3_present"="green","empty"="grey"),  limit = c("empty","1/3_present","2/3_present", "3/3_present"), drop = F)
 
-#Loop over plates and make a plot for each plate
-
-## plot_list = list()
-## for (i in unique(plate_data$Assay_Plate_Code)) {
-##   plate_data %>%
-##     filter(Assay_Plate_Code == i)  -> data
-##   data %>% 
-##     full_join(empty_wells %>% transform(colnum = as.factor(colnum), rownum = as.factor(rownum))) %>%
-##     mutate(color = ifelse(is.na(color),"empty",color)) %>%
-##     mutate(Control_Code = ifelse(is.na(Control_Code),"empty",Control_Code)) -> data
-##    # mutate(colnum = fct_reorder(colnum, desc(as.numeric(colnum)))) 
-  
-##   data = data[order(as.numeric(as.character(data$colnum))),]
-##   cols = c("0/3_present" = "red","1/3_present" = "yellow","2/3_present"="orange","3/3_present"="green","empty"="grey")
-##   plot = ggplot(data, aes(y = fct_reorder(rownum,desc(rownum)),x = fct_reorder(colnum,sort(as.numeric(data$colnum))),shape = Control_Code)) + 
-##     geom_point(aes(colour = color), size =12) + 
-##     scale_shape_manual(values = c("empty"=16,"control"=17,"sample"=16), limits = c("empty","control","sample"))+
-##     scale_color_manual(values = cols)  +
-##     labs(x= i, y = "ASIC_plate_map")+
-##     ggtitle("ASIC plate map")
-  
-##  # plot_list[[i]] = plot
-##   print(plot)
-## }
-
-  # .d <- plate_data %>% filter(Assay_Batch_Code=="Batch1" & Assay_Plate_Code=="P1")
-  x1 <- plate_data %>% group_by(Assay_Batch_Code, Assay_Plate_Code) %>% do({
-    data <- as_tibble(.) %>%   full_join(empty_wells %>% transform(colnum = as.character(colnum), rownum = as.character(rownum))) %>%
-      mutate(color = ifelse(is.na(color),"empty",color)) %>%
-      mutate(Control_Code = ifelse(is.na(Control_Code),"empty",Control_Code)) 
+    x1 <- dat %>% 
+           group_by(Assay_Batch_Code, Assay_Plate_Code) %>% 
+           do({
+               plot_plate(., "replace_na(ASIC_status, 'empty')", asic_color, title="ASIC plate map")
+           })
     
-    data = data[order(as.numeric(as.character(data$colnum))),]
-    cols = c("0/3_present" = "red","1/3_present" = "yellow","2/3_present"="orange","3/3_present"="green","empty"="grey")
-    plot = ggplot(data, aes(y = fct_reorder(rownum,desc(rownum)),x = fct_reorder(colnum,sort(as.numeric(data$colnum))),shape = Control_Code)) + 
-      geom_point(aes(colour = color), size =12) + 
-      scale_shape_manual(values = c("empty"=16,"control"=17,"sample"=16), limits = c("empty","control","sample"))+
-      scale_color_manual(values = cols)  +
-      labs(x= sprintf("Batch: %s, Plate: %s", .$Assay_Batch_Code[1], .$Assay_Plate_Code[1]), y = "ASIC_plate_map")+
-      ggtitle("ASIC plate map")
-    
-  # plot_list[[i]] = plot
-    print(plot)
-    data
-  }) 
+    qc_color <- scale_color_manual(name="Overall QC", values = c("fail" = "red","pass"="green","empty"="grey"),  limit = c("empty","fail","pass"), drop = F)
 
-#gridExtra::grid.arrange(grobs = plot_list, nrow = 6, ncol =2, newpage = T)
-##ggplotGrob(plot_list)
+    # Plot for QC
+    x2 <- dat %>% 
+           group_by(Assay_Batch_Code, Assay_Plate_Code) %>% 
+           do({
+               plot_plate(., "replace_na(overall_qc, 'empty')", qc_color, title="Overall QC plate map")
+           })
 
-#plate map for BM2s
+    # make control plate plot
+    control_color <-  scale_color_manual(name="Control status", values = c("pos_pass"='green',"pos_fail"='red',"neg_pass"='blue',"neg_fail"='yellow',"sample" ='white',"empty" ='grey'),  limit = c("pos_pass","pos_fail","neg_pass","neg_fail","sample","empty"), drop = F)
 
-  plate_data <- manifest %>%
-  mutate(barcode = paste0(BC1,BC2)) %>%
-  full_join(specimen_control_defs %>% select(Control_Code,Control_type) %>%
-               mutate(Owner_Sample_ID = Control_Code),
-             by = "Owner_Sample_ID") %>%
-  inner_join(detailed_pn_matrix_for_report, by = c("barcode","Owner_Sample_ID")) %>%
-  gather(type,status,starts_with("B2M")) %>%
-  mutate(count = ifelse(status == "pos",1,0)) %>%
-  select(barcode,Owner_Sample_ID,Assay_SIC,Assay_Well_ID,Assay_Plate_Code,Control_Code,type,status,count, Assay_Batch_Code) %>%
-  group_by(barcode,Owner_Sample_ID,Assay_Plate_Code, Assay_Batch_Code) %>%
-  mutate(sum_count = sum(count)) %>%
-  select(-count)%>%
-  spread(type,status) %>%
-  mutate(color = "white") %>% 
-  mutate(color = ifelse(sum_count == 0,"0/2_present",color)) %>%
-  mutate(color = ifelse(sum_count == 1,"1/2_present",color)) %>%
-  mutate(color = ifelse(sum_count == 2,"2/2_present",color)) %>%
-  mutate(Control_Code = ifelse(is.na(Control_Code),"sample","control")) %>% 
-  separate(Assay_Well_ID,c("rownum","colnum"),sep =1) %>%
-  drop_na() 
-  plate_data <-as.data.frame(plate_data)
-#Create empty wells
-well_num = seq(1,12,length.out = 12)  
-well_ID = LETTERS[1:8]
-empty_wells = as.data.frame(expand.grid(rownum=well_ID, colnum= well_num,stringsAsFactors = F))
-
-
-#Plot the plates
-## plot_list = list()
-## for (i in unique(plate_data$Assay_Plate_Code)) {
-##   plate_data %>%
-##     filter(Assay_Plate_Code == i)  -> data
-##   data %>%
-##     full_join(empty_wells %>% transform(colnum = as.character(colnum), rownum = as.factor(rownum))) %>%
-##     mutate(color = ifelse(is.na(color),"empty",color)) %>%
-##     mutate(Control_Code = ifelse(is.na(Control_Code),"empty",Control_Code)) -> data
- 
-##   data = data[order(as.numeric(as.character(data$colnum))),]
-##   cols = c("0/2_present"="red","1/2_present"="yellow","2/2_present" = "green","empty"="grey")
-##   plot = ggplot(data, aes(x = fct_reorder(colnum,sort(as.numeric(data$colnum))),y = fct_reorder(rownum,desc(rownum)),shape = Control_Code)) + 
-##     geom_point(aes(colour = color), size =12) +
-##     scale_shape_manual(values = c("empty"=16,"control"=17,"sample"=16), limit = c("empty","control","sample"), drop = F) +
-##     scale_color_manual(values = cols, limits = c("0/2_present","1/2_present","2/2_present","empty"),  drop = F) +
-##     labs(x= i, y = "TypeSeqHPV_plate_data")+
-##     ggtitle("B2M/Human_control plate map")
-    
-
-##   print(plot)
-##  # plot_list[[i]] = plot
-  
-## }
-
-  x2 <- plate_data %>% group_by(Assay_Batch_Code, Assay_Plate_Code) %>% do({
-    data <- as_tibble(.) %>%
-    full_join(empty_wells %>% transform(colnum = as.character(colnum), rownum = as.factor(rownum))) %>%
-    mutate(color = ifelse(is.na(color),"empty",color)) %>%
-    mutate(Control_Code = ifelse(is.na(Control_Code),"empty",Control_Code))
-
-    data = data[order(as.numeric(as.character(data$colnum))),]
-    cols = c("0/2_present"="red","1/2_present"="yellow","2/2_present" = "green","empty"="grey")
-    plot = ggplot(data, aes(x = fct_reorder(colnum,sort(as.numeric(data$colnum))),y = fct_reorder(rownum,desc(rownum)),shape = Control_Code)) + 
-      geom_point(aes(colour = color), size =12) +
-      scale_shape_manual(values = c("empty"=16,"control"=17,"sample"=16), limit = c("empty","control","sample"), drop = F) +
-      scale_color_manual(values = cols, limits = c("0/2_present","1/2_present","2/2_present","empty"),  drop = F) +
-      labs(x= sprintf("Batch: %s, Plate: %s", .$Assay_Batch_Code[1], .$Assay_Plate_Code[1]), y = "TypeSeqHPV_plate_data")+
-      ggtitle("B2M/Human_control plate map")
-      
-
-    print(plot)
-    data
-  })
-# print(gridExtra::grid.arrange(grobs = plot_list,  nrow = 6,ncol = 2, newpage = T))
-
-
-
-# B2M_plot(manifest,detailed_pn_matrix_for_report)
-
-# Plate map for all batch-controls
-
-# batch_control_plot = function(control_results_final,specimen_control_defs,detailed_pn_matrix_for_report) {
-
-plate_data <- control_for_report %>%
-              select(barcode, control_result, Control_type) %>%
-              full_join(detailed_pn_matrix_for_report %>% select(barcode)) %>%
-              mutate_if(is.factor, ~ as.character(.)) %>%
-              mutate(control_result = ifelse(is.na(control_result), "sample", control_result)) %>%
-              mutate(Control_type = ifelse(is.na(Control_type), as.character("sample"), Control_type)) %>%
-              inner_join(manifest %>% mutate(barcode = paste0(BC1,BC2)),by = c("barcode")) %>% 
-              select(barcode,Owner_Sample_ID,control_result, Control_type, Assay_Plate_Code,Assay_Well_ID, Assay_Batch_Code) %>%
-              mutate(color = "white") %>%
-              mutate(color = ifelse(control_result == "pass" & Control_type == "pos","pos_pass",color)) %>%
-              mutate(color = ifelse(control_result == "fail" & Control_type == "pos",'pos_fail',color)) %>%
-              mutate(color = ifelse(control_result == "pass" & Control_type == "neg",'neg_pass',color)) %>%
-              mutate(color = ifelse(control_result == "fail" & Control_type == "neg",'neg_fail',color)) %>%
-              mutate(color =ifelse(control_result == "sample","sample",color)) %>%
-              mutate(Control_code = ifelse(Control_type == "sample","sample",Owner_Sample_ID)) %>%
-              separate(Assay_Well_ID,c("rownum","colnum"),sep =1) %>%
-              drop_na() 
-
-# creat empty wells
-well_num = seq(1,12,length.out = 12)  
-well_ID = LETTERS[1:8]
-empty_wells = as.data.frame(expand.grid(rownum=well_ID, colnum= well_num,stringsAsFactors = F))
-
-
-## plot_list = list()
-## for (i in unique(plate_data$Assay_Plate_Code)) {
-##   plate_data %>%
-##     filter(Assay_Plate_Code == i)  -> data
-  
-##   data %>% 
-##     full_join(empty_wells %>% transform(colnum = as.character(colnum), rownum = as.factor(rownum))) %>% 
-##     mutate(color = ifelse(is.na(color),"empty",color)) %>%
-##     mutate(Owner_Sample_ID = ifelse(is.na(Owner_Sample_ID), "empty",Owner_Sample_ID)) %>%
-##     mutate(control_result = ifelse(is.na(control_result),"empty",control_result)) %>%
-##     mutate(Control_type =ifelse(is.na(Control_type), "empty",Control_type))%>%
-##     mutate(Control_code = ifelse(is.na(Control_code),"empty",Control_code)) %>%
-##     mutate(Control_code = ifelse(Control_code != "sample" & Control_code != "empty","control",Control_code)) -> data
-  
-##   data = data[order(as.numeric(as.character(data$colnum))),]
-##   cols = c("pos_pass"='green',"pos_fail"='red',"neg_pass"='blue',"neg_fail"='yellow',"sample" ='white',"empty" ='grey')
-##   plot = ggplot(data, aes(x = fct_reorder(colnum,sort(as.numeric(data$colnum))),y = fct_reorder(rownum,desc(rownum)), shape = Control_code)) + 
-##     geom_point(aes(colour = color), size =12) +
-##     scale_shape_manual(name = "Shape", values = c("sample"= 16,"control" = 17,"empty"= 16),limits = c("sample","control","empty"), drop = FALSE) +
-##     scale_color_manual(name = "Color",values = cols, limits = c("pos_pass","pos_fail","neg_pass","neg_fail","sample","empty")) +
-##     labs(x= i, y = "TypeSeqHPV_plate_data") +
-##     scale_x_discrete(limits = rev(levels(data$colnum))) +
-##     ggtitle("All batch control plate map")
-  
-##  # plot_list[[i]] = plot
-  
-##   print(plot)
-  
-## }
-
-#print(gridExtra::grid.arrange(grobs =plot_list,  nrow = 6))
-  
-  x3 <- plate_data %>% group_by(Assay_Batch_Code, Assay_Plate_Code) %>% do({
-    data <- as_tibble(.) %>% full_join(empty_wells %>% transform(colnum = as.character(colnum), rownum = as.factor(rownum))) %>% 
-      mutate(color = ifelse(is.na(color),"empty",color)) %>%
-      mutate(Owner_Sample_ID = ifelse(is.na(Owner_Sample_ID), "empty",Owner_Sample_ID)) %>%
-      mutate(control_result = ifelse(is.na(control_result),"empty",control_result)) %>%
-      mutate(Control_type =ifelse(is.na(Control_type), "empty",Control_type))%>%
-      mutate(Control_code = ifelse(is.na(Control_code),"empty",Control_code)) %>%
-      mutate(Control_code = ifelse(Control_code != "sample" & Control_code != "empty","control",Control_code))
-
-      data = data[order(as.numeric(as.character(data$colnum))),]
-      cols = c("pos_pass"='green',"pos_fail"='red',"neg_pass"='blue',"neg_fail"='yellow',"sample" ='white',"empty" ='grey')
-      plot = ggplot(data, aes(x = fct_reorder(colnum,sort(as.numeric(data$colnum))),y = fct_reorder(rownum,desc(rownum)), shape = Control_code)) + 
-        geom_point(aes(colour = color), size =12) +
-        scale_shape_manual(name = "Shape", values = c("sample"= 16,"control" = 17,"empty"= 16),limits = c("sample","control","empty"), drop = FALSE) +
-        scale_color_manual(name = "Color",values = cols, limits = c("pos_pass","pos_fail","neg_pass","neg_fail","sample","empty")) +
-        labs(x= sprintf("Batch: %s, Plate: %s", .$Assay_Batch_Code[1], .$Assay_Plate_Code[1]), y = "TypeSeqHPV_plate_data") +
-        scale_x_discrete(limits = rev(levels(data$colnum))) +
-        ggtitle("All batch control plate map")
-      
-    # plot_list[[i]] = plot
-      
-      print(plot)
-      data
-  })
-
+    x3 <- dat %>% 
+           group_by(Assay_Batch_Code, Assay_Plate_Code) %>% 
+           do({
+               plot_plate(., "replace_na(control_status, 'empty')", control_color, title="All batch control plate map")
+           })
 }
-
-#}
