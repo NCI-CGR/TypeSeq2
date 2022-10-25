7f64cbe3815ed00647587fd544e4af58bf7cd013
weizhu365 <zhuw10@nih.gov>
2022-10-25 09:22:52 -0400

    feat(R/plate_summary.R):export table as csv

--- R/plate_summary.R
+++ R/plate_summary.R
@@ -1,33 +1,44 @@
 #' plate_summary
 #'
 
-plate_summary <- function(samples_only_for_report, is_clinical=F,for_batch=F){
+plate_summary <- function(samples_only_for_report, is_clinical=F,for_batch=F, output_prefix=NULL){
 
     require(pander)
 
     vv <- ifelse(! for_batch, "Assay_Batch_Code", "Project") %>% rlang::sym()
 
-    t3 <- samples_only_for_report %>% 
-        group_by(!!vv, Assay_Plate_Code) %>% 
-        summarise( 
-            hpv_pos_perc = fmt_perc(sum(Num_Types_Pos >0, na.rm=T) / n(), digits=1 ),
-            plate_total_reads = scales::comma(sum(total_reads, na.rm = TRUE)),
-            number_of_samples = n(), 
-            num_samples_failed = sum(overall_qc == "fail", na.rm=T)
-        ) %>% 
-        dplyr::select(!!vv, 
-            `Assay Plate Code` = Assay_Plate_Code,
-            `HPV % Positive` = hpv_pos_perc,
-            `total reads` = plate_total_reads,
-            `# samples total` = number_of_samples,
-            `# samples failed` = num_samples_failed,
-        )
+    # if samples_only_for_report is empty
+    # Error: Evaluation error: object 'total_reads' not found.
+    t4=data.frame()
+    if(nrow(samples_only_for_report)==0){
+        
 
-    t3 %>% when(
-                is_clinical ~  select(., -`HPV % Positive`),
-                ~ .
-            ) %>%
-            pandoc.table(style = "multiline", caption = "Assay Plate Performance", split.cells = 12)
+    }else{
+        t4 <- samples_only_for_report %>% 
+            group_by(!!vv, Assay_Plate_Code) %>% 
+            summarise( 
+                hpv_pos_perc = fmt_perc(sum(Num_Types_Pos >0, na.rm=T) / n(), digits=1 ),
+                plate_total_reads = scales::comma(sum(total_reads, na.rm = TRUE)),
+                number_of_samples = n(), 
+                num_samples_failed = sum(overall_qc == "fail", na.rm=T)
+            ) %>% 
+            dplyr::select(!!vv, 
+                `Assay Plate Code` = Assay_Plate_Code,
+                `HPV % Positive` = hpv_pos_perc,
+                `total reads` = plate_total_reads,
+                `# samples total` = number_of_samples,
+                `# samples failed` = num_samples_failed,
+            ) %>% when(
+                    is_clinical ~  select(., -`HPV % Positive`),
+                    ~ .
+                )
+    }
 
+    t4 %>% pandoc.table(style = "multiline", caption = "Assay Plate Performance", split.cells = 12)
+
+    if(!is.null(output_prefix)){
+        write_csv(t4, sprintf("%s.Table4.csv", output_prefix))
+    }
     
+    return(t4)
 }
