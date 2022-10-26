2fc29d1f5ab862c85bc733d26d4e994230e3668a
weizhu365 <zhuw10@nih.gov>
2022-10-26 09:53:30 -0400

    feat(workflows/TypeSeq2.R):include new output files in TypeSeq2_outputs.zip

--- workflows/TypeSeq2.R
+++ workflows/TypeSeq2.R
@@ -174,7 +174,8 @@ drake::make(ion_plan)
 new_fn <- renaming_read_summary(readd(user_files))
 
 ### Compress file here
-system("zip -r TypeSeq2_outputs.zip *.read_summary.csv *results.csv *QC_report.pdf *.batch_metrics_summary.csv *.run_metrics.csv Scaled_min-filters.csv control_definitions barcode_file grouping_file typing_manifest *.full.csv ")
+# include more files here
+system(sprintf("zip -r TypeSeq2_outputs.zip *.read_summary.csv *results.csv *QC_report.pdf *.batch_metrics_summary.csv *.run_metrics.csv Scaled_min-filters.csv control_definitions barcode_file grouping_file typing_manifest *.full.csv %s.Table*.csv %s.*_plot_data.csv", get_output_prefix(), get_output_prefix() ))
 
 if( ! is.na(command_line_args$is_clinical) ){
     system("zip -r TypeSeq2_outputs.laboratory.zip *.read_summary.csv *control_results.csv *samples_only_matrix_results.csv *failed_samples_pn_matrix_results.csv *-pn_matrix_results.laboratory.csv *laboratory_report.pdf Scaled_min-filters.csv control_definitions barcode_file grouping_file typing_manifest *.laboratory.csv ")
